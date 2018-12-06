2eb056391b03f68ecec7fb2bee5012c925a09386
PiethonCoder <austinthemichaud@gmail.com>
2018-12-06 12:51:11 -0500

    Add files via upload

--- doc/script.js
+++ doc/script.js
@@ -1,1 +1,17 @@
-$("")
+$("#show_tips1").click(function() {
+    $("#tips1").show(1000);
+})
+
+$("#show_tips2").click(function() {
+    $("#tips2").show(1000);
+})
+
+
+$("#show_tips3").click(function() {
+    $("#tips3").show(1000);
+})
+
+
+$("#show_tips4").click(function() {
+    $("#tips4").show(1000);
+})
